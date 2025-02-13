40d5bf7ffdba0028b26cc3c209d60889e429921e
Pietro-Freitas <pietrocardosof@gmail.com>
2025-02-13 15:07:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const imagensSala1 = document.querySelectorAll('img.sala1');
+
+    imagensSala1.forEach(imagem => {
+        imagem.addEventListener('click', function() {
+            alert('Essa foto, do dia 06/02, foi tirada justamente para esse trabalho de PW || ');
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const imagensSala2 = document.querySelectorAll('img.sala2');
+
+    imagensSala2.forEach(imagem => {
+        imagem.addEventListener('click', function() {
+            alert('Uma foto que muito provavelmente foi tirada para o instagram da escola. Alguns concentrados na tarefa, mas outros nem tanto');
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const imagensSala3 = document.querySelectorAll('img.sala3');
+
+    imagensSala3.forEach(imagem => {
+        imagem.addEventListener('click', function() {
+            alert('Dia que ocorreu a MOBFOG. Quando lancei meu foguete, todos comecamos a correr indo buscá-lo. Foi um dia bem divertido');
+        });
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const imagensSala4 = document.querySelectorAll('img.sala4');
+
+    imagensSala4.forEach(imagem => {
+        imagem.addEventListener('click', function() {
+            alert('Mesma foto que a primeira, mas tirada de um ângulo diferente. Fomos todos correndo para frente da sala pra tirarmos essa foto o quanto antes e terminar a licao do professor Humberto');
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const imagensSala5 = document.querySelectorAll('img.sala5');
+
+    imagensSala5.forEach(imagem => {
+        imagem.addEventListener('click', function() {
+            alert('Mais uma foto repetida que usariam para postar no instagram da escola');
+        });
+    });
+});
+
